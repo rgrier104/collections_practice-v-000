5f952f29831705dcba686a7c090b78a83b1c5812
Rachel Grier <rgrier104@gmail.com>
2018-08-31 00:30:43 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,48 @@
 
+def sort_array_asc(array)
+  array.sort
+end
+ def sort_array_desc(array)
+   array.sort {|x,y| -(x <=> y)}
+ end
+
+def sort_array_char_count(array)
+  array.sort {|x,y| x.length <=> y.length}
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  return array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each do |string|
+    string[2] = "$"
+  end
+end
+
+def find_a(array)
+  array.select do |string|
+    string.start_with?("a")
+  end
+end
+
+def sum_array(array)
+  array.inject do |sum, n|
+    sum + n
+  end
+end
+
+def add_s(array)
+  array.each_with_index.collect do |element, index|
+    if index == 1
+      element
+    else
+     element + "s"
+   end
+ end
+end
