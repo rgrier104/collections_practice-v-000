df2ea59aad8aef7b673a862c88dcc74e19918e8c
Rachel Grier <rgrier104@gmail.com>
2018-08-31 00:30:26 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -38,5 +38,11 @@ def sum_array(array)
 end
 
 def add_s(array)
-  array.each_with_index.collect{|element, index| element + "s"}
+  array.each_with_index.collect do |element, index|
+    if index == 1
+      element
+    else
+     element + "s"
+   end
+ end
 end
